c0b1e0e36c6b26867c6923a4bb691f7eeb12e317
Maxim <defilak@mail.ru>
2022-06-10 00:20:36 +0300

    cheta sdelal

--- vkp80ii.js
+++ vkp80ii.js
@@ -1,9 +1,8 @@
 const EJECTOR = [0x1D, 0x65]
-const EJECTOR_RETRACT = 0x02 // Втянуть
-const EJECTOR_EJECT = 0x05 // Извлечь
+const EJECTOR_RETRACT = 0x02 
+const EJECTOR_EJECT = 0x05 
 
 const TOTAL_CUT = [0x1B, 0x69]
-
 
 const SET_PRINT_MODE = [0x1B, 0x21]
 
@@ -27,22 +26,27 @@ const JUSTIFY_MODE = {
 const LEFT_MARGIN = [0x1D, 0x4C]
 const PRINTING_AREA_WIDTH = [0x1D, 0x57]
 
-//////////
-class vkp80ii {
+const BARCODE_WIDTH = [0x1d, 0x77]
+const BARCODE_HEIGHT = [0x1D, 0x68]
+
+const BARCODE_PRINT = [0x1D, 0x6B]
+const CODE128 = 70
+const CODE128_CHARS = {
+    CODE_A: [0x7B, 0x41],
+    CODE_B: [0x7B, 0x42],
+    CODE_С: [0x7B, 0x43],
+}
+
+const HRI_POSITION = [0x1D, 0x48]
+const HRI_NONE = 0;
+const HRI_ABOVE = 1;
+const HRI_BELOW = 2;
+
+class vkp80ii extends EscPos {
     endpoint = 2
 
     constructor(device) {
-        this.device = device
-    }
-
-    send(...arr) {
-        this.device.transferOut(this.endpoint, new Uint8Array(arr))
-        return this
-    }
-
-    ascii(text) {//?
-        let encoder = new TextEncoder()
-        this.send(encoder.encode(text))
+        super(device)
     }
 
     print(text = '', mode = false) {//0x1B 0x64
@@ -56,6 +60,43 @@ class vkp80ii {
 
     println(text = '', mode = false) {
         this.print(text + '\n', mode)
+    }
+
+    printCode128(barcode) {
+        this.justify(JUSTIFY_MODE.LEFT)
+        this.printMode(PRINT_MODE.ITALIC | PRINT_MODE.FONT_B)
+        this.println(barcode)
+
+        this.justify(JUSTIFY_MODE.CENTER)
+        this.leftMargin(0)
+
+        this.barcodeWidth(2)
+        this.barcodeHeight(255)
+        this.hriPos(HRI_NONE)
+
+        this.barcodePrint(barcode, CODE128, CODE128_CHARS.CODE_A)
+    }
+
+    printOneTicket(cb) {
+        this.eject()
+        cb(this)
+        this.totalCut()
+    }
+}
+
+class EscPos {
+    constructor(device) {
+        this.device = device
+    }
+
+    send(...arr) {
+        this.device.transferOut(this.endpoint, new Uint8Array(arr))
+        return this
+    }
+
+    ascii(text) {//?
+        let encoder = new TextEncoder()
+        this.send(encoder.encode(text))
     }
 
     printMode(mode) {
@@ -74,8 +115,6 @@ class vkp80ii {
         this.send(...PRINTING_AREA_WIDTH, ...Bytes.short2ba(n))
     }
 
-    /////
-
     eject() {
         this.send(...EJECTOR, EJECTOR_EJECT)
     }
@@ -88,10 +127,36 @@ class vkp80ii {
         this.send(...TOTAL_CUT)
     }
 
-    printOneTicket(cb) {
-        this.eject()
-        cb(this)
-        this.totalCut()
+    /**
+     * n MODULE WIDTH ( mm )
+     * 1 0.125
+     * 2 0.25
+     * 3 0.375
+     * 4 0.5
+     * 5 0.625
+     * 6 0.75
+     */
+    barcodeWidth(n = 3) {
+        this.send(...BARCODE_WIDTH, Math.min(Math.max(n, 1), 6))
+    }
+
+    barcodeHeight(n = 162) {
+        this.send(...BARCODE_HEIGHT, Math.min(Math.max(n, 1), 255))
+    }
+
+    barcodePrint(text, type = CODE128, charset = CODE128_CHARS.CODE_A) {
+        let encoder = new TextEncoder()
+        this.send(
+            ...BARCODE_PRINT,
+            type,
+            text.length + charset.length,
+            ...charset,
+            ...encoder.encode(text)
+        )
+    }
+
+    hriPos(pos) {
+        this.send(...HRI_POSITION, pos)
     }
 }
 
